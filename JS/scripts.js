20918031629adc6ce15273ff0539fc98b9156e58
kkwekk <mikhailstepanov167@gmail.com>
2024-09-30 22:57:23 +0500

    Сцена кинотеатра

---
+++ JS/scripts.js
@@ -0,0 +1,14 @@
+const schemeSVG = document.querySelector(".scheme-svg");
+const totalPriceTag = document.querySelector(".price-total")
+let cost = 620;
+let totalPrice = 0;
+schemeSVG.addEventListener('click', (event) => {
+if(!event.target.classList.contains('booked'))
+{
+    event.target.classList.toggle('active');
+    let totalSeats = schemeSVG.querySelectorAll(".active").length
+    totalPrice =  totalSeats * cost;
+    totalPriceTag.textContent = totalPrice;
+    console.log();
+}
+});
