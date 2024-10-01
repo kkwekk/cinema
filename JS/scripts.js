f881ac6a7f2e4c7e8278003baa8113b09b14e343
kkwekk <mikhailstepanov167@gmail.com>
2024-10-01 15:16:53 +0500

    готовый кинотеатр

---
+++ JS/scripts.js
@@ -0,0 +1,20 @@
+const schemeSVG = document.querySelector(".scheme-svg");
+const totalPriceTag = document.querySelector(".price-total");
+const menuButton = document.querySelector(".m-menu");
+const menu = document.querySelector(".menu")
+let cost = 620;
+let totalPrice = 0;
+schemeSVG.addEventListener('click', (event) => {
+if(!event.target.classList.contains('booked'))
+{
+    event.target.classList.toggle('active');
+    let totalSeats = schemeSVG.querySelectorAll(".active").length
+    totalPrice =  totalSeats * cost;
+    totalPriceTag.textContent = totalPrice;
+    console.log();
+}
+});
+menuButton.addEventListener('click', () =>{
+    console.log("Кликнули по меню");
+    menu.classList.toggle("is-open");
+})
